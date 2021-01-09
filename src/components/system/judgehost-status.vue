37e129b3e399edbb723ceaec03798ecbd1e364a0
Keadin Zhou <34813154+KeadinZhou@users.noreply.github.com>
2021-01-09 23:14:21 +0800

    judgehost status

--- src/components/system/judgehost-status.vue
+++ src/components/system/judgehost-status.vue
@@ -1,15 +1,15 @@
 <template>
   <title-box-frame title="评测机器状态/Judgehost">
     <template v-slot:content>
-      <a-card-grid v-for="(judgehost, index) in judgehostData" style="width:25%;text-align: center" :key="'judgehost'+index">
-        <b class="judgehost-name">{{ judgehost.name }}</b> / {{ judgehost.ip }} <br>
-        {{ judgehost.last_time }} <br> <br>
-        <template v-if="judgehost.status">
-          <a-badge color="green" status="processing"></a-badge>状态正常
-        </template>
-        <template v-else>
-          <a-badge color="red" status="error"></a-badge>状态异常
-        </template>
+      <loading-box-frame v-if="loading"></loading-box-frame>
+      <a-card-grid v-else v-for="(judgehost, index) in judgehostData" style="width:25%;text-align: center" :key="'judgehost'+index">
+        <b class="judgehost-name">{{ judgehost.judgehostName }}</b> / Judgehost-{{ $store.state.fixZero(index+1) }} <br>
+        {{ judgehost.heartbeatTime }} <br> <br>
+        <a-badge
+            :color="statusCalc(judgehost.status).color"
+            :status="statusCalc(judgehost.status).status"
+            :text="statusCalc(judgehost.status).text">
+        </a-badge>
       </a-card-grid>
     </template>
   </title-box-frame>
@@ -17,64 +17,71 @@
 
 <script>
 import TitleBoxFrame from '@/components/frame/title-box-frame'
+import LoadingBoxFrame from '@/components/frame/loading-box-frame'
 export default {
   name: "judgehost-status",
   components: {
-    'title-box-frame': TitleBoxFrame
+    'title-box-frame': TitleBoxFrame,
+    'loading-box-frame': LoadingBoxFrame
   },
   data() {
     return {
-      judgehostData: [
-        {
-          name: 'Judgehost-01',
-          ip: '10.66.28.6',
-          last_time: this.$store.state.dateFormat(new Date(), true),
-          status: 1
-        },
-        {
-          name: 'Judgehost-02',
-          ip: '10.66.28.6',
-          last_time: this.$store.state.dateFormat(new Date(), true),
-          status: 1
-        },
-        {
-          name: 'Judgehost-03',
-          ip: '10.66.28.6',
-          last_time: this.$store.state.dateFormat(new Date(), true),
-          status: 1
-        },
-        {
-          name: 'Judgehost-04',
-          ip: '10.66.28.6',
-          last_time: this.$store.state.dateFormat(new Date(), true),
-          status: 1
-        },
-        {
-          name: 'Judgehost-05',
-          ip: '10.66.28.6',
-          last_time: this.$store.state.dateFormat(new Date(), true),
-          status: 1
-        },
-        {
-          name: 'Judgehost-06',
-          ip: '10.66.28.6',
-          last_time: this.$store.state.dateFormat(new Date(), true),
-          status: 1
-        },
-        {
-          name: 'Judgehost-07',
-          ip: '10.66.28.6',
-          last_time: this.$store.state.dateFormat(new Date(), true),
-          status: 0
-        },
-        {
-          name: 'Judgehost-08',
-          ip: '10.66.28.6',
-          last_time: this.$store.state.dateFormat(new Date(), true),
-          status: 0
-        },
-      ]
+      loading: false,
+      judgehostData: []
     }
+  },
+  methods: {
+    statusCalc(s) {
+      switch (s) {
+        case -1:
+          return {
+            color: '',
+            status: 'error',
+            text: '离线'
+          }
+        case 0:
+          return {
+            color: '',
+            status: 'default',
+            text: '未知'
+          }
+        case 1:
+          return {
+            color: 'green',
+            status: 'processing',
+            text: '在线'
+          }
+        case 2:
+          return {
+            color: '',
+            status: 'warning',
+            text: '正忙'
+          }
+      }
+    },
+    getData() {
+      let that = this
+      that.loading = true
+      that.$http.get(that.$store.state.host + '/judgehost/status')
+          .then(data => {
+            if (data.data.code === 200) {
+              let Data = data.data.data
+              that.judgehostData = Data
+            } else {
+              that.$message.error(data.data.msg)
+              that.$store.commit('errorPage', data.data.code)
+            }
+          })
+          .catch(() => {
+            that.$message.error('系统错误')
+          })
+          .finally(() => {
+            that.loading = false
+          })
+    }
+  },
+  created() {
+    this.getData()
   }
 }
 </script>
